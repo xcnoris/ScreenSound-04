0c7cf17612eabd800656d4a4281ed678891ae2c4
xcnoris <augustobriel12@gmail.com>
2024-04-06 17:47:21 -0300

    Adicionando a class LinqFilter, onde tem o a funcao FiltrarTodosOsGenerosMusicais, que nos tras todos os generos musicas

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        //                                  - seleciona algo dentro da lista - Nao pode valores iguais e retorna como lista
+        var todosOsGenerosMusicais = musicas.Select(musicas => musicas.Genero).Distinct().ToList();
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($" - {genero}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSound_04.Modelos;
 using System.Text.Json;
+using ScreenSound_04.Filtros;
 
 using (HttpClient client = new HttpClient())
 {
@@ -7,7 +8,7 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        musicas[0].ExibirDetalhesDaMusica();
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {
