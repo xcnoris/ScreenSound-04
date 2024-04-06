a0aa92d09a9c9888b00bc5cf3a84819a8a864b72
xcnoris <augustobriel12@gmail.com>
2024-04-06 19:18:34 -0300

    Criamos uma nova class chamada MusicasPreferidas

---
+++ Modelos/MusicasPreferidas.cs
@@ -0,0 +1,25 @@
+﻿namespace ScreenSound_04.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+
+    private List<Musica> ListadeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome) { 
+        Nome= nome;
+        ListadeMusicasFavoritas = new List<Musica>();
+    }
+    public void AdicionarMusicaFavoritas(Musica musica)
+    {
+        ListadeMusicasFavoritas.Add(musica);
+    }
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Musicas Favoritas do(a) >>> {Nome}");
+        foreach (var musica in ListadeMusicasFavoritas)
+        {
+            Console.WriteLine($"-- {musica.Nome} -- {musica.Artista}");
+        }
+    }
+}
