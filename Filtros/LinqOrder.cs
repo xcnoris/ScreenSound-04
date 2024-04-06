97e1cace48c2c75dc63989ac2b0bd5337dad88e5
xcnoris <augustobriel12@gmail.com>
2024-04-06 18:11:27 -0300

    Criamos a class LinqOrder, onde filtramos apenas a propriedade artista da class Musica, e ordenamos de forma ancedente

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,25 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExbirListaDeArtistaOrdenados(List<Musica> musicas)
+    {
+        /*
+            OrderBy() = Ordena de forma cendente. EXP: ABC
+            OrderByDecending() = Ordena de forma decendente. EXP: CBA
+
+            Select() = Seleciona apenas a propriedade artistas
+            Distinct() = Não deixa retorna mais de uma vez o mesmo nome de artista
+            ToList() = Retorna como lista(nao é necessario nesse caso)
+        */
+        var artistasOrdenados = musicas.OrderBy(musicas => musicas.Artista).Select(musicas => musicas.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artistas ordenados\n");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@
 using System.Text.Json;
 using ScreenSound_04.Filtros;
 
+
 using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExbirListaDeArtistaOrdenados(musicas);
+
     }
     catch (Exception ex)
     {
